2d3913dc5ac1e5692dad61d577b1f76b5927d7c4
Puriya MONFARET <puriya_monfaret@yahoo.com>
2025-08-29 18:01:42 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,28 @@
-const CACHE = "puri-honumara-v5";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest"
-  // ikonların varsa: "./icons/p-192.png", "./icons/p-512.png"
-];
+// sw.js
+const CACHE = "puri-honumara-v9";
+const ASSETS = ["./","./index.html","./manifest.json"];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
+  self.clients.claim();
 });
-
-self.addEventListener("fetch", (e) => {
-  const req = e.request;
-  // only GET
-  if (req.method !== "GET") return;
-  e.respondWith(
-    caches.match(req).then(res => {
-      return (
-        res ||
-        fetch(req)
-          .then((net) => {
-            // runtime cache
-            const copy = net.clone();
-            caches.open(CACHE).then((c) => c.put(req, copy));
-            return net;
-          })
-          .catch(() => caches.match("./"))
-      );
-    })
-  );
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith((async()=>{
+    try{
+      const net = await fetch(e.request);
+      const cache = await caches.open(CACHE);
+      cache.put(e.request, net.clone());
+      return net;
+    }catch(_){
+      const hit = await caches.match(e.request);
+      return hit || new Response("Offline", {status:503});
+    }
+  })());
 });
